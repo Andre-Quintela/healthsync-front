5c99d73c25a222f51c4ec2ac276243cbeb4381dd
André <andre.lsquintela@gmail.com>
2024-10-27 02:15:06 -0300

    Atualização da página de SignUp

--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -3,47 +3,49 @@ import FlaskAlt from '../../assets/Flask_alt.svg';
 
 const SignUp = () => {
     return (
-        <div className='container'>
-            <div className='info-section'> 
-                <div className='info'>
-                    <img src={FlaskAlt} alt='Flask Icon'/>
-                    <div>
-                        <h3>Agende consulstas de forma fácil</h3>        
-                        <p>Agende consultas de forma rápido e fácil</p>
-                    </div>
-                </div>
-
-                <div className='info'>
-                    <img src={FlaskAlt} alt='Flask Icon'/>
-                    <div>
-                        <h3>Agende consulstas de forma fácil</h3>
-                        <p>Agende consultas de forma rápido e fácil</p>
-                    </div>
-                </div>
-
-                <div className='info'>
-                    <img src={FlaskAlt} alt='Flask Icon'/>
-                    <div>
-                        <h3>Agende consulstas de forma fácil</h3>
-                        <p>Agende consultas de forma rápido e fácil</p>
-                    </div>
+        <div className="container">
+        <div className="info-section">
+            <div className="info">
+                <img src={FlaskAlt}></img>
+                <div>
+                    <h3>Marque suas consultas</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
                 </div>
             </div>
-
-            <div className='form-section'>
-                <form>
-                    <h2>Registre-se no<br></br><span>HealthSync</span></h2>
-                    <div>
-                        <input type='text' placeholder='Primeiro nome' />
-                        <input type='text' placeholder='Último nome' />
-                    </div>
-                    <input type='email' placeholder='Email' />
-                    <input type='password' placeholder='Senha' />
-                    <input type='password' placeholder='Confirme sua senha' />
-                    <button>Registre-se</button>
-                </form>
+            <div className="info">
+                <img src={FlaskAlt}></img>
+                <div>
+                    <h3>Receba seus exames</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
             </div>
-        </div>   
+            <div className="info">
+                <img src={FlaskAlt}></img>
+                <div>
+                    <h3>Facilite sua saúde</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </div>
+            </div>
+        </div>
+        <div className="form-section">
+            <h2>Registre-se no<br></br><span>HealthSync</span></h2>
+            <form>
+                <input type="text" placeholder="Primeiro nome" required />
+                <input type="text" placeholder="Sobrenome" required />
+                <input type="date" placeholder="Data de nascimento" required />
+                <input type="email" placeholder="Email" required />
+                <input type="tel" placeholder="Número de telefone" required />
+                <input type="text" placeholder="Endereço" required />
+                <div className="checkbox">
+                    <input type="checkbox" id="terms" required />
+                    <label htmlFor="terms">
+                        Eu aceito todos os <a href="#">Termos de condições</a> e <a href="#">Politicas de privacidade</a> do aplicativo HealthSync.
+                    </label>
+                </div>
+                <button type="submit">Registre-se</button>
+            </form>
+        </div>
+    </div>
     );
 };
 
